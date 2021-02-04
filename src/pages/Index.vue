22118077c05100629d2c9169ada094e5dfcf26c5
多喝热水长生不老 <7804417+william408@user.noreply.gitee.com>
2021-02-05 02:07:10 +0800

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,118 @@
+<template>
+  <Layout>
+    <!-- Page Header -->
+    <!-- style="background-image: url('/img/home-bg.jpg')" -->
+    <header
+      class="masthead"
+      :style="{
+        backgroundImage: `url(http://localhost:1337${general.image.url})`
+      }"
+    >
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+              <h1>{{ general.title }}</h1>
+              <span class="subheading">{{ general.subtitle }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-preview" v-for="edge in $page.posts.edges" :key="edge.node.id">
+            <g-link :to="'/post/' + edge.node.id">
+              <h2 class="post-title">
+                {{ edge.node.title }}
+              </h2>
+              <!-- <h3 class="post-subtitle">
+                Problems look mighty small from 150 miles up
+              </h3> -->
+            </g-link>
+            <p class="post-meta">Posted by
+              <a href="#">{{ edge.node.author }}</a>
+              on {{ edge.node.created_at }}</p>
+            <p>
+              <span v-for="tag in edge.node.tags" :key="tag.id">
+                <g-link :to="'/tag/' + tag.id">{{ tag.title }}</g-link>
+                &nbsp;&nbsp;
+              </span>
+            </p>
+          </div>
+          <hr>
+          <!-- Pager -->
+          <!-- <div class="clearfix">
+            <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+          </div> -->
+          <Pager :info="$page.posts.pageInfo" />
+        </div>
+      </div>
+    </div>
+  </Layout>
+</template>
+<page-query>
+  query ($page: Int) {
+    posts: allStrapiPost(perPage: 2, page: $page) @paginate {
+      totalCount,
+      pageInfo {
+        totalPages
+        currentPage
+      }
+      edges {
+        node {
+          id,
+          title,
+          content,
+          created_at,
+          tags {
+            id,
+            title
+          },
+          author
+        }
+      }
+    }
+    
+    general: allStrapiGeneral {
+      edges {
+        node {
+          id,
+          title,
+          subtitle,
+          image {
+            url
+          }
+        }
+      }
+    }
+  }
+</page-query>
+<script>
+import { Pager } from 'gridsome';
+
+export default {
+  name: 'HomePage',
+  metaInfo: {
+    title: 'Hello, world!'
+  },
+  components: {
+    Pager
+  },
+  computed: {
+    general() {
+      return this.$page.general.edges[0].node
+    }
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
